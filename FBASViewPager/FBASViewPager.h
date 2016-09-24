46a5bb1b3955edb06f2e385195d5b483c16e7f8a
serxlee <serx.lee@gmail.com>
2016-09-24 22:12:23 +0800

    Initial Commit

---
+++ FBASViewPager/FBASViewPager.h
@@ -0,0 +1,19 @@
+//
+//  FBASViewPager.h
+//  FBASViewPager
+//
+//  Created by Serx on 2016/9/24.
+//  Copyright © 2016年 SerxLee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FBASViewPager.
+FOUNDATION_EXPORT double FBASViewPagerVersionNumber;
+
+//! Project version string for FBASViewPager.
+FOUNDATION_EXPORT const unsigned char FBASViewPagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FBASViewPager/PublicHeader.h>
+
+
